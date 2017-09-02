a7a675b93a5a794ed69f77cd3d1dd81cdb407a51
Sourabh Jajoria <Sourabh Jajoria>
2017-09-02 15:22:09 +0530

    Separating downloader from UI

---
+++ Downloader.py
@@ -0,0 +1,172 @@
+import sys
+import os
+import re
+from bs4 import BeautifulSoup
+from urllib import urlopen
+from urlparse import parse_qs
+from PyQt4 import QtCore, QtGui 
+
+def handle_video_download(playlist_url, start_index, stream_type, download_type, app):
+    playlist_section = get_playlist_section(playlist_url, download_type)
+    youtube_domain = "https://www.youtube.com"
+    videos_to_download = []
+    if playlist_section != None:
+    	playlist_video_urls = get_playlist_video_urls(playlist_section,youtube_domain)
+    else:
+    	playlist_video_urls = [playlist_url]
+    videos_to_be_downloaded = get_videos_to_be_dowloaded(
+        playlist_url, playlist_video_urls, start_index)
+    for url in videos_to_be_downloaded:
+    	download_video(url,youtube_domain,stream_type, app)
+
+
+def get_playlist_section(playlist_url, download_type):
+    if download_type == 'single':
+        return None
+    response = urlopen(playlist_url)
+    parsed_page = BeautifulSoup(response,'html.parser')
+    playlist = parsed_page.find('div',class_='playlist-videos-container yt-scrollbar-dark yt-scrollbar')
+    return playlist
+
+def get_playlist_video_urls(playlist,youtube_domain):
+    videos = playlist.find_all('li',class_='yt-uix-scroller-scroll-unit')
+    video_urls = []
+    for video in videos:
+        QtCore.QCoreApplication.processEvents()
+        video_element = video.find('a',class_='playlist-video')
+        video_url = youtube_domain + video_element['href']
+        video_urls.append(unicode(video_url))
+    return video_urls
+
+def get_videos_to_be_dowloaded(playlist_url, video_urls, start_index):
+	if start_index == 'origin' or len(video_urls) < 2:
+		videos_to_be_downloaded = video_urls
+	else:
+		current_video_url, _current_video_indexing = playlist_url.split('&',1)
+		videos_to_be_downloaded = get_videos_starting_current_url(current_video_url, video_urls)
+	return videos_to_be_downloaded    
+
+
+def get_videos_starting_current_url(current_video_url, video_urls):
+	index_of_current_video = get_index_of_current_video(current_video_url, video_urls)
+	if index_of_current_video != None:
+		videos_from_current_url = video_urls[index_of_current_video :]
+	else:
+		videos_from_current_url = []
+	return videos_from_current_url
+
+def get_index_of_current_video(current_video_url, video_urls):
+    number_of_videos = len(video_urls)
+    for video_index in range(0, number_of_videos):
+        QtCore.QCoreApplication.processEvents()
+        if current_video_url in video_urls[video_index]:
+            return video_index
+    return None
+
+def write_file(file_name,stream, file_size, app):
+    with open(file_name, 'ab', 0) as f:
+        while True:
+            QtCore.QCoreApplication.processEvents()
+            one_kb = stream.read(1024)
+            if not one_kb:
+                break
+            file_size += 1024
+            app.update_download_progress(file_size)
+            f.write(one_kb)
+
+def get_download_link(file_info):
+    url_map = parse_qs(file_info['url_encoded_fmt_stream_map'][0])
+    url_map_split = url_map['url']
+    download_url = url_map_split[0]
+    return download_url
+
+def get_audio_download_link(file_info):
+    adaptive_url_map = parse_qs(file_info['adaptive_fmts'][0])
+    url_map_split = adaptive_url_map['url']
+    audio_url = url_map_split[-1]
+    return audio_url
+
+def get_stream_url(file_info, stream_type):
+        if stream_type != 'audio':
+            download_url = get_download_link(file_info)
+        else:
+            download_url = get_audio_download_link(file_info) 
+        return download_url
+
+def get_video_size(download):
+    download_info = download.info()
+    download_size = int(download_info.getheaders("Content-Length")[0])
+    return download_size
+
+def resume_download(file_size, file_name, download_size, download_url, app):
+    print("Resuming download...")
+    resume_url = "{0}&range={1}-{2}".format(download_url,file_size,download_size)
+    download = urlopen(resume_url)
+    write_file(file_name,download, file_size, app)
+
+def get_file_info(url,youtube_domain):
+    normal_video_id_split = url.split('?v=')
+    shortened_video_id_split = url.split('.be/')
+    if len(normal_video_id_split) > 1:
+        video_id = normal_video_id_split[1]
+    elif len(shortened_video_id_split) > 1:
+        video_id = shortened_video_id_split[1]
+    else:
+        raise Exception('Video URL not comprehensible')
+    file_info_url = youtube_domain+'/get_video_info?video_id='+video_id
+    QtCore.QCoreApplication.processEvents()
+    response_file = urlopen(file_info_url)
+    file_info_coded = response_file.read()
+    file_info = parse_qs(file_info_coded)
+    if 'title' not in file_info:
+        raise Exception('Video is copyrighted or restricted')
+    title = file_info['title'][0]
+    return file_info, title
+
+def get_video_name(title, stream_type):
+    if stream_type != 'audio':
+        content_name = title+'.mp4'
+    else:
+        content_name = title+'.mp3'        
+    file_name = re.sub(r'[^(a-z)(A-Z)(0-9).]','_',content_name)
+    return file_name
+
+def start_download(file_name, download_size, download_file, download_url, app):
+    if os.path.isfile(file_name):
+        file_size = os.path.getsize(file_name)
+        if file_size < download_size:
+            app.update_status('Resuming download..')
+            resume_download(file_size,file_name,download_size,download_url, app)
+    else:
+        write_file(file_name,download_file, 0, app)
+    app.show_success_message("Download complete!")
+    app.update_status('Completed!')
+    print('Download complete!')
+
+def attempt_download(file_info,title,stream_type, app):
+    app.update_status('Downloading..')
+    retry = 0
+    while retry < 20:
+        QtCore.QCoreApplication.processEvents()
+        download_url = get_stream_url(file_info, stream_type)                       
+        download_file = urlopen(download_url)
+        download_size = get_video_size(download_file)
+        if download_size > 0:
+            break
+        retry += 1
+    if download_size == 0:
+        app.update_status('Copyrighted!')
+        raise Exception('Video is copyrighted or restricted')
+    file_name = get_video_name(title, stream_type)
+    app.add_table_item(file_name, download_size, 0)
+    print("Downloading:{0} \nSize: {1} bytes".format(file_name,download_size))            
+    start_download(file_name, download_size, download_file, download_url, app)    
+
+def download_video(url,youtube_domain,stream_type, app):
+    try:
+        file_info,title = get_file_info(url,youtube_domain)
+        attempt_download(file_info,title,stream_type, app)
+    except Exception as e:
+        error_message = e.args
+        app.show_error_message(error_message)
+        print('Download failed! ',e.args)
